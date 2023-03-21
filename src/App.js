501607a2c40a562f7fe1115dc59e08a17e21cf16
Sandeep0322 <apple@Apples-MacBook-Pro-21.local>
2023-03-21 15:02:08 +0530

    latest code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import { Button } from 'react-bootstrap';
 const starkwareCrypto = require('@starkware-industries/starkware-crypto-utils');
 const StarkExAPI = require('@starkware-industries/starkex-js/browser');
+const Web3 = require('web3')
 
 function App() {
   const [account, setAccount] = react.useState("");
@@ -12,6 +13,7 @@ function App() {
   const starkExAPI = new StarkExAPI({
     endpoint: 'https://perpetual-playground-v2.starkex.co'
   });
+  const web3 = new Web3(window.ethereum)
 
   const handleMetaMask = async () => {
   
@@ -48,26 +50,30 @@ function App() {
     return publicKey1X.toString(16);
   }
 
-  // const getStarkSign = async (msgHash) => {
-  //   console.log("msgHash :", msgHash)
-  //   console.log('nonBIP32 key generation');
-  //   const signer = provider.getSigner()
-  //   let message = "We need you sign this message to create you a guardian link account"
-  //   let flatsig = await signer.signMessage(message);
-  //   let sig = ethers.utils.splitSignature(flatsig);
-  //   const grinded = starkwareCrypto.keyDerivation.grindKey(sig.r, starkwareCrypto.ec.n)
-  //   console.log("grind Key :", grinded)
-  //   let datas = starkwareCrypto.ec.keyFromPrivate(grinded, 'hex');
-  //   let sign = starkwareCrypto.sign(datas, msgHash)
-  //   console.log(sign)
-  //   return sign
-  // }
+  const getStarkSign = async () => {
+    const msgHash = "d48540481cfe9cf19f362f6da55a8b43ada3bc89e5340afe1c0a792733186c"
+    console.log("msgHash :", msgHash)
+    console.log('nonBIP32 key generation');
+    const signer = provider.getSigner()
+    let message = "We need you sign this message to create you a guardian link account"
+    let flatsig = await signer.signMessage(message);
+    let sig = ethers.utils.splitSignature(flatsig);
+    const grinded = starkwareCrypto.keyDerivation.grindKey(sig.r, starkwareCrypto.ec.n)
+    console.log("grind Key :", grinded)
+    let datas = starkwareCrypto.ec.keyFromPrivate(grinded, 'hex');
+    let sign = starkwareCrypto.sign(datas, msgHash)
+    let r = await web3.utils.toHex(sign.r)
+    let s = await web3.utils.toHex(sign.s)
+    console.log(r, s)
+    return sign
+  }
 
   const transferToken = async () => {
     // let senderStarkKey = await getStarkKey();
     // let receiverStarkKey = "0x35f08479cc3c90e5dae64cc8c1f537a8875a04e3045ef0635972f48256599d1";
-    let txId = await starkExAPI.gateway.getFirstUnusedTxId();
-    console.log("txId",txId);
+    // console.log("starkex", starkExAPI.gateway)
+    // let txId = await starkExAPI.gateway.getFirstUnusedTxId();
+    // console.log("txId",txId);
   }
 
   return (
@@ -76,7 +82,8 @@ function App() {
       {account == "" && <Button onClick={handleMetaMask}>connect</Button>}<br></br>
       {account != "" && <Button className="button-85" onClick={getStarkKey}>starkKey</Button>}
       {starkKey && <p>StarkKey - {starkKey}</p>}
-      {account != "" && <Button className="button-85" onClick={transferToken}>transfer</Button>}
+      {/* {account != "" && <Button className="button-85" onClick={transferToken}>transfer</Button>} */}
+      {account != "" && <Button className="button-85" onClick={getStarkSign}>starkSign</Button>}
     </div>
   );
 }
